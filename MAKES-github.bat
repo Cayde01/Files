564d38009e3e3666616728bd1e62917521c082eb
Cayde01 <84088250+Cayde01@users.noreply.github.com>
2022-12-30 07:29:40 +0100

    Update MAKES-github.bat

--- MAKES-github.bat
+++ MAKES-github.bat
@@ -1,14 +1,9 @@
 md "C:\K\"
 subst K: C:\K
-md "K:\DOWNLOADS\"
-md "K:\WWW\"
 md "K:\X\"
 subst X: C:\K\X
+
+md "K:\DOWNLOADS\"
 md "K:\WORK\ACTIVE-JOB"
-md "K:\WORK\ACTIVE-SOCIAL"
-md "K:\WORK\ACTIVE-VIDEOS"
 md "K:\WORK\COVERS_RAW"
 md "K:\WORK\COVERS_DONE"
-md "K:\WORK\RAR-DONE"
-md "K:\WORK\UPLOAD
-md "K:\WORK\ZZZ_DELETE_ZZZ"
